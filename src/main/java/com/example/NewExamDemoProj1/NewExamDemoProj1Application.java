5ad01bc2da416a5ba679bcf40bce2e187ccc12fe
waseem tufail <wasimtuff@gmail.com>
2025-01-07 06:21:53 +0100

    Update NewExamDemoProj1Application.java
    did mistake to check the bild in jenkins

--- src/main/java/com/example/NewExamDemoProj1/NewExamDemoProj1Application.java
+++ src/main/java/com/example/NewExamDemoProj1/NewExamDemoProj1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewExamDemoProj1Application {
 
-	public static void main(String[] args) {
+	public staticc void main(String[] args) {
 		SpringApplication.run(NewExamDemoProj1Application.class, args);
 	}
 

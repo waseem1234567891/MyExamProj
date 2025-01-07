64e85437640c3db1bbfd6b2f73c11131403f8961
waseem tufail <wasimtuff@gmail.com>
2025-01-07 06:23:55 +0100

    Update NewExamDemoProj1Application.java
    now correcting my mistake to see jenkins build

--- src/main/java/com/example/NewExamDemoProj1/NewExamDemoProj1Application.java
+++ src/main/java/com/example/NewExamDemoProj1/NewExamDemoProj1Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NewExamDemoProj1Application {
 
-	public staticc void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(NewExamDemoProj1Application.class, args);
 	}
 
